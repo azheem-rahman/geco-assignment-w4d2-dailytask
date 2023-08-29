7b47b733a449919759d20c185a589f16bea9d53a
Azheem Rahman <azheem.rahman@gmail.com>
2023-08-30 00:45:20 +0800

    created array of objects and updated second index

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactHooks from "./ReactHooks";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <ReactHooks />
+    </div>
+  );
+};
+
+export default App;
